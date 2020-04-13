1c7ccef646be16e968f796febaf785aa1b73a426
Ioannis Gkikas <funkyfisch@gmail.com>
2020-04-13 19:15:31 +0200

    4

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,5 +1,4 @@
 import { shallowMount } from "@vue/test-utils";
-<<<<<<< HEAD
 
 describe("Example", () => {
   it("renders props.msg when passed", () => {
@@ -9,16 +8,5 @@ describe("Example", () => {
     //   });
     //   expect(wrapper.text()).toMatch(msg);
     expect("success").toEqual("success");
-=======
-import ProfileTile from "@/components/ProfileTile.vue";
-
-describe("ProfileTile.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "";
-    const wrapper = shallowMount(ProfileTile, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
->>>>>>> 5c468c65cad73f9dbba90ac774b40cc746007b03
   });
 });
